f775c69165550d7d603d26650f92448f918c5d32
Ol-create <oluola96@gmail.com>
2023-06-11 07:02:20 +0100

    group the tests

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,16 +1,18 @@
 const lib = require('../lib')
 
-test("absolute - if the number is positive the return should be positive", () => {
-    let result = lib.absolute(1)
-    expect(result).toBe(1)
+describe('absolute', () => {
+    it("absolute - if the number is positive the return should be positive", () => {
+      let result = lib.absolute(1);
+      expect(result).toBe(1);
+    });
+
+    it("absolute - if the number is negative the return should be positive", () => {
+      let result = lib.absolute(-1);
+      expect(result).toBe(1);
+    });
+
+    it("absolute - if the number is zore the return should be zero", () => {
+      let result = lib.absolute(0);
+      expect(result).toBe(0);
+    });
 })
-
-test("absolute - if the number is negative the return should be positive", () => {
-  let result = lib.absolute(-1);
-  expect(result).toBe(1);
-});
-
-test("absolute - if the number is zore the return should be zero", () => {
-  let result = lib.absolute(0);
-  expect(result).toBe(0);
-});
