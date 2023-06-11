ec17d3858349d75b8d4689bfe6e1e265158b240a
Ol-create <oluola96@gmail.com>
2023-06-11 06:55:37 +0100

    add test for number

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,3 +1,16 @@
-test("my first test", () => {
-    throw new Error("The world should get better");
+const lib = require('../lib')
+
+test("absolute - if the number is positive the return should be positive", () => {
+    let result = lib.absolute(1)
+    expect(result).toBe(1)
 })
+
+test("absolute - if the number is negative the return should be positive", () => {
+  let result = lib.absolute(-1);
+  expect(result).toBe(1);
+});
+
+test("absolute - if the number is zore the return should be zero", () => {
+  let result = lib.absolute(0);
+  expect(result).toBe(0);
+});
